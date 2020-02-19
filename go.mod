b79d545133d8983df5910f563ae2c78ec88ea873
Enrico Spurio <enrico14@gmail.com>
2020-02-19 09:33:32 +0000

    add config

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/enrico5b1b4/go-github-actions-playground
+
+go 1.13
--- hello_test.go
+++ hello_test.go
@@ -2,13 +2,13 @@ package play_test
 
 import (
 	"testing"
-  
-  play "github.com/enrico5b1b4/go-github-actions-playground"
+
+	play "github.com/enrico5b1b4/go-github-actions-playground"
 )
 
 func TestHello(t *testing.T) {
-  hello := play.Hello("Enrico")
-  if hello != "Hello Enrico" {
-    t.Fail()
-  }
+	hello := play.Hello("Enrico")
+	if hello != "Hello Enrico" {
+		t.Fail()
+	}
 }
